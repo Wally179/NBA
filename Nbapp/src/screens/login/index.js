898a894f430c80ce89f415e44cf819360586cbd0
Wally179 <wallace-179@hotmail.com>
2023-11-01 20:22:09 -0300

    Tela login 3/4

--- Nbapp/src/screens/login/index.js
+++ Nbapp/src/screens/login/index.js
@@ -1,6 +1,7 @@
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
 import styles from "./style";
+import { FontAwesome } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
@@ -21,11 +22,46 @@ export default function Login() {
       <Image source={require("../../../img/Icone.png")} style={styles.logo} />
       <View style={styles.conteudo}>
         <Text style={styles.login}>Login</Text>
-        <Text style={styles.login}>Email</Text>
-        <TextInput />
-        <Text style={styles.login}>Senha</Text>
-        <TextInput />
-        <Text style={styles.login}>Esqueceu a senha</Text>
+        <Text style={styles.text}>Email</Text>
+        <TextInput style={styles.imput} />
+        <Text style={styles.text}>Senha</Text>
+        <TextInput style={styles.imput} />
+        <TouchableOpacity
+          onPress={() => console.log("Botão senha pressionado")}
+        >
+          <Text style={styles.esqsenha}>Esqueceu a senha</Text>
+        </TouchableOpacity>
+        <View style={styles.linha}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => console.log("Botão 1 pressionado")}
+          >
+            <Image
+              source={require("../../../img/google-icon.png")}
+              style={styles.logo2}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => console.log("Botão 2 pressionado")}
+          >
+            <FontAwesome name="facebook-square" size={25} color="#395185" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => console.log("Botão 3 pressionado")}
+          >
+            <FontAwesome name="apple" size={25} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.bot}>
+        <TouchableOpacity
+          onPress={() => console.log("Botão login pressionado")}
+          style={styles.btnlogin}
+        >
+          <Text style={styles.btn}>Entrar</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- Nbapp/src/screens/login/style.js
+++ Nbapp/src/screens/login/style.js
@@ -7,13 +7,6 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
   },
-  login: {
-    color: "#fff",
-  },
-  logo: {
-    height: "15%",
-    width: "15%",
-  },
   back: {
     width: "100%",
     position: "absolute",
@@ -24,7 +17,73 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
   },
-  conteudo: { width: "100%" },
+  login: {
+    color: "#fff",
+    fontSize: 36,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
+    paddingTop: 10,
+  },
+  logo: {
+    height: "15%",
+    width: "15%",
+  },
+  logo2: {
+    height: 20,
+    width: 20,
+  },
+  conteudo: {
+    width: "100%",
+    padding: 16,
+    height: "50%",
+    marginTop: "10%",
+  },
+  imput: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "white",
+    padding: 8,
+  },
+  button: {
+    backgroundColor: "white",
+    width: 45,
+    height: 45,
+    borderRadius: 5,
+    marginRight: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  linha: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  esqsenha: {
+    width: "100%",
+    color: "#fff",
+    textAlign: "right",
+  },
+  btnlogin: {
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: "white",
+    padding: 10,
+    width: "35%",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 60,
+  },
+  btn: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  bot: {
+    height: "30%",
+    width: "100%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
 });
 
 export default styles;
